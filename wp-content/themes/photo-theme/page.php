f9dc2d321a6ae33640036f6e3b7315a1b8bad305
absentees <scottblissett@gmail.com>
2015-09-20 14:34:10 +1000

    things

--- wp-content/themes/photo-theme/page.php
+++ wp-content/themes/photo-theme/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="contact-info">
+<div class="content-wrapper">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php the_content(); ?>
   <?php endwhile; else : ?>
